baa2592081f85ad68c2c7a0160221464bc555e95
volodymyrhudyma <volodymyrhudyma@gmail.com>
2020-04-21 20:11:29 +0200

    Some stylings

--- src/components/BlogPostItem/BlogPostItem.js
+++ src/components/BlogPostItem/BlogPostItem.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Wrapper } from "./styles"
+import { Wrapper, Date, Title, Excerpt, ReadMore } from "./styles"
 
 const BlogPostItem = ({ post }) => (
   <Wrapper>
-    <div>{post.frontmatter.date}</div>
-    <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+    <Date>{post.frontmatter.date}</Date>
+    <Link to={post.fields.slug}>
+      <Title>{post.frontmatter.title}</Title>
+    </Link>
+    <Excerpt>{post.excerpt}</Excerpt>
+    <Link to={post.fields.slug}>
+      <ReadMore>Read more</ReadMore>
+    </Link>
   </Wrapper>
 )
 
--- src/components/BlogPostItem/styles.js
+++ src/components/BlogPostItem/styles.js
@@ -1,3 +1,15 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div``
+
+export const Date = styled.div`
+  margin-bottom: 0.25rem;
+`
+
+export const Title = styled.h2``
+
+export const Excerpt = styled.div``
+
+export const ReadMore = styled.div`
+  margin-top: 0.25rem;
+`
