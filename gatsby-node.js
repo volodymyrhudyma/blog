a6899256a8e74676b84538460547c2cd11407a43
volodymyrhudyma <volodymyrhudyma@gmail.com>
2020-04-21 20:45:25 +0200

    Add pagination to blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog.js`)
   const result = await graphql(`
     {
       allMarkdownRemark(
@@ -24,11 +23,31 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  const posts = result.data.allMarkdownRemark.edges
+
+  // Create blog post page
+  const blogPostTemplate = path.resolve(`src/templates/blog.js`)
+  posts.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: blogPostTemplate,
       context: {}, // additional data can be passed via context
+    })
+  })
+
+  // Create blog-list pages
+  const postsPerPage = 3
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/blog-list.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
     })
   })
 }
--- src/components/BlogPostItem/styles.js
+++ src/components/BlogPostItem/styles.js
@@ -1,15 +1,20 @@
 import styled from "styled-components"
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  margin-bottom: 1.45rem;
+`
 
 export const Date = styled.div`
   margin-bottom: 0.25rem;
 `
 
-export const Title = styled.h2``
+export const Title = styled.h2`
+  display: inline-block;
+`
 
 export const Excerpt = styled.div``
 
 export const ReadMore = styled.div`
   margin-top: 0.25rem;
+  display: inline-block;
 `
