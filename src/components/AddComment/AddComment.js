f4d0d7c9727fe069b25dd789f0d0b840fa9a4370
volodymyrhudyma <volodymyrhudyma@gmail.com>
2020-04-25 10:02:59 +0200

    Add response

--- src/components/AddComment/AddComment.js
+++ src/components/AddComment/AddComment.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react"
 import axios from "axios"
 
+import { Input, Textarea, FlashMessage, Button, Wrapper } from "./styles"
+
 const AddComment = () => {
+  const [flashMessage, setFlashMessage] = useState(null)
   const [comment, setComment] = useState({
     slug: "slug",
     name: "",
@@ -20,8 +23,15 @@ const AddComment = () => {
           message: comment.message,
         },
       })
+      setFlashMessage({
+        type: "success",
+        message: "Thank you, appreciate it",
+      })
     } catch (e) {
-      console.log(e)
+      setFlashMessage({
+        type: "error",
+        message: "Something went wrong",
+      })
     }
   }
 
@@ -33,25 +43,34 @@ const AddComment = () => {
   }
 
   return (
-    <form onSubmit={onSubmit} action={process.env.STATICMAN_URL}>
-      <input name="slug" type="hidden" value={comment.slug} />
-      <input
-        name="name"
-        type="text"
-        placeholder="Name"
-        onChange={onChange}
-        value={comment.name}
-        required
-      />
-      <textarea
-        name="message"
-        placeholder="Message"
-        onChange={onChange}
-        value={comment.message}
-        required
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <Wrapper>
+      <h3>Responses</h3>
+      <form onSubmit={onSubmit} action={process.env.STATICMAN_URL}>
+        {flashMessage && (
+          <FlashMessage type={flashMessage.type}>
+            {flashMessage.message}
+          </FlashMessage>
+        )}
+        <Input name="slug" type="hidden" value={comment.slug} />
+        <Input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={onChange}
+          value={comment.name}
+          required
+        />
+        <Textarea
+          name="message"
+          placeholder="Message"
+          onChange={onChange}
+          value={comment.message}
+          rows="3"
+          required
+        />
+        <Button>Submit</Button>
+      </form>
+    </Wrapper>
   )
 }
 
---
+++ src/components/AddComment/styles.js
@@ -0,0 +1,37 @@
+import styled, { css } from "styled-components"
+
+const shared = css`
+  width: 100%;
+  border: none;
+  outline: none;
+  padding: 0.5rem 0;
+  margin-bottom: 1rem;
+`
+
+export const Input = styled.input`
+  ${shared};
+`
+
+export const Textarea = styled.textarea`
+  resize: none;
+  ${shared};
+`
+
+export const FlashMessage = styled.div`
+  color: ${({ type }) => (type === "success" ? "#1ca086" : "red")};
+  border: 1px solid;
+  padding: 1rem;
+  margin-bottom: 1rem;
+`
+
+export const Button = styled.button`
+  background-color: #1ca086;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 2rem;
+  cursor: pointer;
+`
+
+export const Wrapper = styled.div`
+  border-top: 1px solid lightgray;
+`
