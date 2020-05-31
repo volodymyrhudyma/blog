eb2a539a04d0e65cdafa67be8e3674065e646d7b
volodymyrhudyma <volodymyrhudyma@gmail.com>
2020-05-31 15:36:30 +0200

    Add post image

--- src/components/BlogPostItem/BlogPostItem.js
+++ src/components/BlogPostItem/BlogPostItem.js
@@ -1,20 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Wrapper, BlogTitle, BlogDate } from "./styles"
+import { Wrapper, BlogTitle, BlogDate, Image, JST } from "./styles"
 
 const BlogPostItem = ({ post }) => (
   <Wrapper>
-    <BlogTitle>
-      <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-    </BlogTitle>
-    <BlogDate>{post.frontmatter.date}</BlogDate>
-    <p style={{ margin: 0 }}>
-      {post.frontmatter.teaser}
-      <Link style={{ marginLeft: "0.25rem" }} to={post.fields.slug}>
-        Read more
-      </Link>
-    </p>
+    <Image>
+      {post.frontmatter.title}
+      <JST src="./favicon/favicon.png" />
+    </Image>
+    <div>
+      <BlogTitle>
+        <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+      </BlogTitle>
+      <BlogDate>{post.frontmatter.date}</BlogDate>
+      <p style={{ margin: 0 }}>
+        {post.frontmatter.teaser}
+        <Link style={{ marginLeft: "0.25rem" }} to={post.fields.slug}>
+          Read more
+        </Link>
+      </p>
+    </div>
   </Wrapper>
 )
 
--- src/components/BlogPostItem/styles.js
+++ src/components/BlogPostItem/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const Wrapper = styled.div`
   margin-bottom: 2rem;
+  display: flex;
 `
 
 export const BlogTitle = styled.h2`
@@ -13,4 +14,26 @@ export const BlogDate = styled.span`
   font-size: 0.85rem;
   margin-bottom: 0.5rem;
   display: inline-block;
+`
+
+export const Image = styled.div`
+  width: 200px;
+  background-color: rgb(250, 250, 250);
+  margin-right: 1rem;
+  flex-shrink: 0;
+  color: #000;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  padding: 1rem;
+  position: relative;
+`
+
+export const JST = styled.img`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  right: 0.5rem;
+  bottom: 0.5rem;
+  margin: 0;
 `
