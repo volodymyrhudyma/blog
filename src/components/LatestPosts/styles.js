2b971f420790c28ac5a912f13b0bfef32bb52834
Volodymyr Hudyma <volodymyr.hudyma@fortum.com>
2021-07-17 18:29:23 +0200

    Change styles

--- src/components/LatestPosts/styles.js
+++ src/components/LatestPosts/styles.js
@@ -107,7 +107,7 @@ export const PostTitle = styled.h3`
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 4;
+  -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
 `
 
